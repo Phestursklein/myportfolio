7e44e281de0ee612b57ea51829718627952e73ec
OGUNBAYO ABAYOMI FESTUS <phezyklein@gmail.com>
2024-11-07 18:36:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ menuButton.addEventListener('click', () => {
     menuIcon.classList.toggle('rotate-90');
     
 });
+function downloadCV() {
+    const confirmDownload = confirm("Do you want to download the file 'CV ABAYOMI 2024.pdf'?");
+
+    if (confirmDownload) {
+      const link = document.createElement("a");
+      link.href = "./credentials/CV ABAYOMI 2024.pdf"; // Replace with the actual path to the CV file
+      link.download = "CV ABAYOMI 2024.pdf"; // Replace with the desired file name
+      link.click();
+    } else {
+      alert("Download canceled.");
+    }
+  }
